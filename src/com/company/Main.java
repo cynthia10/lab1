b578277e4747829ca2d58d910b91373e00d198e4
cynthia10 <35684320+cynthia10@users.noreply.github.com>
2020-02-09 18:18:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("hello world");
+       class Listing
+       {
+           private int age;
+           private  String name;
+
+        }
+        public String ToSting(); {
+            return null;
+        }
+        public void input(int a, String w) {
+            int age;
+            age = a;
+            String name;
+            name = w;
+        }
+        public void setName()
+    {
+            String name = " ";
+            System.out.println(name);
+
+        }
+        public void setAge(int a)
+        {
+            int Age;
+            Age = 0;
+        }
+        public int getAge() {
+            int Age = 0;
+            return Age;
+
+        }
+        public String getname()
+
+        {
+            String name="";
+            return name;
+
+
+        }
+
+
+
+    }
+}
